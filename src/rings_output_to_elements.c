6d14bb8430820686249086cef424885631e8f0ef
Will M. Farr <w-farr@northwestern.edu>
2010-10-07 22:45:07 -0500

    Added rings_output_to_elements program.

---
+++ src/rings_output_to_elements.c
@@ -0,0 +1,27 @@
+#include"rings.h"
+
+#include<stdio.h>
+
+int
+main() {
+  int read_count = 1;
+
+  while (read_count == 1) {
+    double t;
+    double e, I, O, o;
+    body b;
+
+    read_count = fscanf(stdin, " %lg ", &t);
+    if (read_count != 1) break;
+
+    read_count = read_body(stdin, &b);
+    if (read_count != 1) break;
+
+    elements_from_body(&b, &e, &I, &O, &o);
+
+    fprintf(stdout, "%g %g %g %g %g %g %g\n",
+            t, b.m, b.a, e, I, O, o);
+  }
+
+  return 0;
+}
