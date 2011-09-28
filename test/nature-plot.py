d6d053687d0db731e1f5cb92a49d06af8ca2e04d
Will M. Farr <w-farr@northwestern.edu>
2011-09-28 11:18:56 -0500

    Modifications to nature plot ic for zero spin; added plotting python script.

---
+++ test/nature-plot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as pp
+
+data = np.loadtxt('/tmp/nature.dat')
+
+ei = 3
+ii = 4
+
+ts = data[0::2, 0]
+e1s = data[0::2, ei]
+e2s = data[1::2, ei]
+i1s = data[0::2, ii]
+i2s = data[1::2, ii]
+
+L1s = np.sqrt(1 - e1s**2.0)*np.cos(np.deg2rad(i1s))
+L2s = np.sqrt(1 - e2s**2.0)*np.cos(np.deg2rad(i2s))
+
+pp.subplot(4, 1, 1)
+pp.plot(ts, i1s)
+pp.axhline(90.0)
+
+pp.subplot(4,1,2)
+pp.yscale('log')
+pp.plot(ts, 1 - e1s)
+
+pp.subplot(4,1,3)
+pp.plot(ts, L1s)
+
+pp.subplot(4,1,4)
+pp.plot(ts, L2s)
